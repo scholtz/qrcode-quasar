6e4211c0f4858dd089134f4371635c53833cb944
scholtz <ludko2@gmail.com>
2023-07-06 14:48:13 +0000

    Deploying to gh-pages from @ scholtz/qrcode-quasar@f66dd5cfb297236bce5759dc865ca40ebe281d36 🚀

--- assets/i18n.e74b0476.js
+++ assets/i18n.e74b0476.js
@@ -1,4 +1,4 @@
-import{h as Xe,e as wt,i as Ut,o as Wt,a as Vt,r as Q,c as q,w as ce,F as xt,g as Ye,b as jt,d as Gt,T as $t,f as Ht}from"./index.e0d3aafe.js";/*!
+import{h as Xe,e as wt,i as Ut,o as Wt,a as Vt,r as Q,c as q,w as ce,F as xt,g as Ye,b as jt,d as Gt,T as $t,f as Ht}from"./index.cad0ccc2.js";/*!
   * shared v9.2.2
   * (c) 2022 kazuya kawaguchi
   * Released under the MIT License.
