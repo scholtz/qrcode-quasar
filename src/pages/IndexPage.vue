f66dd5cfb297236bce5759dc865ca40ebe281d36
Ludovit Scholtz <ludko2@gmail.com>
2023-07-06 16:47:43 +0200

    add link to the demo app source code

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -7,36 +7,50 @@
         <a href="https://www.npmjs.com/package/qrcode-vue3">qrcode-vue3</a>
       </div>
       <div>
-        Source code:
-        <a href="https://github.com/scholtz/qrcode-vue3">GitHub</a>
+        QRCode library source code:
+        <a href="https://github.com/scholtz/qrcode-vue3"
+          >https://github.com/scholtz/qrcode-vue3</a
+        >
+      </div>
+      <div>
+        This app source code:
+        <a href="https://github.com/scholtz/qrcode-quasar/"
+          >https://github.com/scholtz/qrcode-quasar/</a
+        >
       </div>
     </div>
-    <QRCodeVue3
-      :width="400"
-      :height="400"
-      :value="data"
-      :key="data"
-      :qr-options="{ errorCorrectionLevel: 'H' }"
-      :image-options="{ hideBackgroundDots: true, imageSize: 0.4, margin: 10 }"
-      :corners-square-options="{ type: 'dot', color: '#2D79C5' }"
-      :corners-dot-options="{ type: undefined, color: '#2D79C5' }"
-      :dots-options="{
-        type: 'dots',
-        color: '#2D79C5',
-        gradient: {
-          type: 'linear',
-          rotation: 0,
-          colorStops: [
-            { offset: 0, color: '#3998F7' },
-            { offset: 1, color: '#24619E' },
-          ],
-        },
-      }"
-      image="usdc.svg"
-      :download="false"
-    />
     <q-input v-model="data" label="Data in QRCode" class="full-width q-pa-lg" />
     {{ data }}
+    <div style="height: 500px">
+      <QRCodeVue3
+        :width="400"
+        :height="400"
+        :value="data"
+        :key="data"
+        :qr-options="{ errorCorrectionLevel: 'H' }"
+        :image-options="{
+          hideBackgroundDots: true,
+          imageSize: 0.4,
+          margin: 10,
+        }"
+        :corners-square-options="{ type: 'dot', color: '#2D79C5' }"
+        :corners-dot-options="{ type: undefined, color: '#2D79C5' }"
+        :dots-options="{
+          type: 'dots',
+          color: '#2D79C5',
+          gradient: {
+            type: 'linear',
+            rotation: 0,
+            colorStops: [
+              { offset: 0, color: '#3998F7' },
+              { offset: 1, color: '#24619E' },
+            ],
+          },
+        }"
+        image="usdc.svg"
+        :download="false"
+      />
+    </div>
   </q-page>
 </template>
 
